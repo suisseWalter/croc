c1d367fc5ef8a2c9b28d305f3fbb554b72c4166f
mccourt killian <killian.mccourt@student-cs.fr>
2025-05-11 01:22:35 +0200

    updated flist to fix openroad issues

--- start_linux.sh
+++ start_linux.sh
@@ -1,3 +1,21 @@
+  1 #!/bin/sh                                                                   
+  2 # Copyright (c) 2024 ETH Zurich and University of Bologna.                  
+  3 # Licensed under the Apache License, Version 2.0, see LICENSE for details.  
+  4 # SPDX-License-Identifier: Apache-2.0                                       
+  5 #                                                                           
+  6 # Authors:                                                                  
+  7 # - Philippe Sauter <phsauter@iis.ee.ethz.ch>                               
+  8 
+  9 env UID=$(id -u) GID=$(id -g) docker compose pull pulp-docker
+ 10 
+ 11 env UID=$(id -u) GID=$(id -g) docker compose run --rm \
+ 12   -e PS1="\[\033[01;32m\]osic:\[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
+    \
+ 13   -e DISPLAY=$DISPLAY \
+ 14   -v /tmp/.X11-unix:/tmp/.X11-unix \
+ 15   pulp-docker
+    
+    
 #!/bin/sh
 # Copyright (c) 2024 ETH Zurich and University of Bologna.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
