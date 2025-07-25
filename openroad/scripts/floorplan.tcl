95cba712167fb71cf4c8c851c5bec0ce15500af8
mccourt killian <killian.mccourt@student-cs.fr>
2025-07-25 14:27:52 +0200

    keeping fix, moving SRAM up

--- openroad/scripts/floorplan.tcl
+++ openroad/scripts/floorplan.tcl
@@ -96,7 +96,7 @@ placeInstance $bank1_sram0 $X $Y R0
 
 # RNG_Bank
 set X [expr $X]
-set Y [expr $Y - $RamSize256x64_H -600]
+set Y [expr $Y - $RamSize256x64_H -15]
 placeInstance $seed_sram $X $Y R0
 
 cut_rows -halo_width_x 2 -halo_width_y 1
